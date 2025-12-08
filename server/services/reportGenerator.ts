7c31c504f65f6e01f4656e37b3c29fef955019c8
vineetdsn <vineetdsn@gmail.com>
2025-12-08 10:36:57 +0530

    Report generator

--- server/services/reportGenerator.ts
+++ server/services/reportGenerator.ts
@@ -1,6 +1,7 @@
 import { Report } from "@shared/schema";
 import PDFKit from "pdfkit";
 import { format } from "date-fns";
+// Need change intypescript
 
 export class ReportGenerator {
   async generatePDF(report: Report): Promise<Buffer> {
